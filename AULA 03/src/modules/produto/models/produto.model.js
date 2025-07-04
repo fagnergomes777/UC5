139ba9cfc94ad09507db1a8d8cddacf38e059a71
fagnergomes777 <phagner@outlook.com>
2025-07-04 19:39:35 +0000

    Primeiro commit

---
+++ AULA 03/src/modules/produto/models/produto.model.js
@@ -0,0 +1,24 @@
+import produtos from '../../../config/database.js'
+
+class ProdutoModel{
+    static cadastrar(){
+
+    }
+    static listarTodos(){
+
+    }
+    static listarPorId(id){
+
+    }
+    static atualizar(){
+
+    }
+    static deletarPorId(){
+
+    }
+    static deletarTodos(){
+
+    }
+}
+
+export default ProdutoModel
